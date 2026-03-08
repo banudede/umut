2b3e957db52749fbc782fd6d4a3d4a686831c295
banudede <banu.dede@hotmail.com>
2026-03-08 18:07:57 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import time
 
 # Sayfa ayarları
-st.set_page_config(page_title=" Umut İçin", page_icon="🥳")
+st.set_page_config(page_title=" Umut İçin", page_icon="❤️")
 
 # Güncellenmiş Başlıklar
-st.title(" Umut İçin Küçük Bir Güzellik 🎉")
+st.title(" Umut İçin Küçük Bir Güzellik 🥳")
 st.subheader("Umut'un Motivasyon İstasyonu 😍")
 st.markdown("---")
 
@@ -23,21 +23,33 @@ if st.button("Sihirli Dokunuşu Başlat"):
     
     if seçim == "Harika hissediyorum!":
         st.balloons() 
-        st.success("Süpersiiiiin! Enerjin dünyayı aydınlatıyor! Yaşasın Umut Neşesiiiiii 😄 ✨")
-        st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHJqdW5kZ2JqZGdqZGdqZGdqZGdqZGdqZGdqZGdqZGdqZGdqJmVwPXYxX2ludGVybmFsX2dpZl9ieV9pZCZjdD1n/l41lTjJp82YyD8wUo/giphy.gif")
+        st.success("Süpersin! Enerjin dünyayı aydınlatıyor! Yaşasın Umut Neşesiiiiiiii 😁 🎉 ✨")
+        
     
     elif seçim == "Biraz yorgunum...":
         st.snow()
-        st.warning("Bazen durgunlaşmak normaldir. Dinlenmek iyidir. ☕")
-       
+        st.warning("Bazen durgunlaşmak normaldir. Dinlenmek iyidir. Keyifli günlerin değerini artırır✨☀️ ☕")
+        
     
     else:
         st.balloons()
         st.success("Hemen enerji yüklüyoruz... Yükleniyor... %100! ⚡")
         st.info("Sen harika bir insansın, Umut! Asla unutma!")
-        st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHJqdW5kZ2JqZGdqZGdqZGdqZGdqZGdqZGdqZGdqZGdqZGdqJmVwPXYxX2ludGVybmFsX2dpZl9ieV9pZCZjdD1n/3o7TKVUn7iM8FMEU24/giphy.gif")
+        
 
-st.markdown("---")
-# Final dokunuşu ve efekt
-st.snow() 
-st.markdown("### BANU'DAN SANA KÜÇÜK BİR ANI :) 💖")
+# FİNAL BUTONU
+    if st.button("Final"):
+        placeholder.empty() # Diğer her şeyi sil
+        
+        # Sadece finalde belirecek efektler ve metinler
+        st.snow()
+        
+        st.markdown("### ✨ Her nefes, evrenden sana yansıyan bir mucize.")
+        time.sleep(1.5)
+        st.markdown("### ✨ İçindeki sessizlikte, ruhunun gerçek gücünü bul.")
+        time.sleep(1.5)
+        st.markdown("### ✨ Yolun ışıkla dolsun, kalbin hep huzur bulsun bebişkosuuuu. Dipnot: Bugün çok duygusalım 🥹😂 ")
+        time.sleep(1.5)
+        st.markdown("---")
+        st.title("BANU'DAN SANA KÜÇÜK BİR ANI :) 💃🏻 ")
+        st.balloons()
